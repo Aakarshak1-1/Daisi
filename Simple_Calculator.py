f432bcf2f0fb977466d65d9a353651b01c6d002c
Aakarshak1-1 <104433678+Aakarshak1-1@users.noreply.github.com>
2022-08-29 22:36:37 +0530

    Update Simple_Calculator.py

--- Simple_Calculator.py
+++ Simple_Calculator.py
@@ -3,49 +3,49 @@ def add(num1, num2):
     return num1+num2
 
 
-def sub(num1, num2):
-    num1,num2=int(num1),int(num2)
-    return num1-num2
+# def sub(num1, num2):
+#     num1,num2=int(num1),int(num2)
+#     return num1-num2
 
 
-def mul(num1, num2):
-    num1,num2=int(num1),int(num2)
-    return num1*num2
+# def mul(num1, num2):
+#     num1,num2=int(num1),int(num2)
+#     return num1*num2
 
 
-def floordiv(num1, num2):
-    num1,num2=int(num1),int(num2)
-    return num1//num2
+# def floordiv(num1, num2):
+#     num1,num2=int(num1),int(num2)
+#     return num1//num2
 
 
-def div(num1, num2):
-    num1,num2=int(num1),int(num2)
-    return num1/num2
+# def div(num1, num2):
+#     num1,num2=int(num1),int(num2)
+#     return num1/num2
 
 
-def modulo(num1, num2):
-    num1,num2=int(num1),int(num2)
-    return num1 % num2
+# def modulo(num1, num2):
+#     num1,num2=int(num1),int(num2)
+#     return num1 % num2
 
 
-def sqrt(num1):
-    num1=int(num1)
-    return num1**0.5
+# def sqrt(num1):
+#     num1=int(num1)
+#     return num1**0.5
 
 
-def square(num1):
-    num1=int(num1)
-    return num1**2
+# def square(num1):
+#     num1=int(num1)
+#     return num1**2
 
 
-def cube(num1):
-    num1=int(num1)
-    return num1**3
+# def cube(num1):
+#     num1=int(num1)
+#     return num1**3
 
 
-def power(num1, num2):
-    num1,num2=int(num1),int(num2)
-    return num1**num2
+# def power(num1, num2):
+#     num1,num2=int(num1),int(num2)
+#     return num1**num2
 
 
 # def compute():
