b18f46f097953356aad9d06b80e477779fd40657
Aakarshak1-1 <104433678+Aakarshak1-1@users.noreply.github.com>
2022-08-29 22:46:56 +0530

    Update Simple_Calculator.py

--- Simple_Calculator.py
+++ Simple_Calculator.py
@@ -1,38 +1,38 @@
-def add(num1: float=0, num2: float=0):
+def add(num1: float=1, num2: float=1):
     return num1+num2
 
-def sub(num1: float=0, num2: float=0):
-    return num1-num2
+# def sub(num1: float=0, num2: float=0):
+#     return num1-num2
 
 
-def mul(num1: float=0, num2: float=0):
-    return num1*num2
+# def mul(num1: float=0, num2: float=0):
+#     return num1*num2
 
 
-def floordiv(num1: float=0, num2: float=0):
-    return num1//num2
+# def floordiv(num1: float=0, num2: float=0):
+#     return num1//num2
 
 
-def div(num1: float=0, num2: float=0):
-    return num1/num2
+# def div(num1: float=0, num2: float=0):
+#     return num1/num2
 
 
-def modulo(num1: float=0, num2: float=0):
-    return num1 % num2
+# def modulo(num1: float=0, num2: float=0):
+#     return num1 % num2
 
 
-def sqrt(num1: float=0):
-    return num1**0.5
+# def sqrt(num1: float=0):
+#     return num1**0.5
 
 
-def square(num1: float=0):
-    return num1**2
+# def square(num1: float=0):
+#     return num1**2
 
 
-def cube(num1: float=0):
-    return num1**3
+# def cube(num1: float=0):
+#     return num1**3
 
 
-def power(num1: float=0, num2: float=0):
-    return num1**num2
+# def power(num1: float=0, num2: float=0):
+#     return num1**num2
 
